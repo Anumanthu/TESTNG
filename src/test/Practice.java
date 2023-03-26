25dc750c385969d94e849448a97b7992cc16c1d8
anumanthu <anumanthunitt@gmail.com>
2023-03-26 16:52:00 +0530

    changes push to git

---
+++ src/test/Practice.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package src.test;
+
+/**
+ * @author akindint
+ *
+ */
+public class Practice {
+
+    /**
+     * {@link}
+     * @param args
+     * @return retrieval
+     * @author accountant
+     * @throws Exception
+     *
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
